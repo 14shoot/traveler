df50220f80c4ec54e482252ac1adb842e776f0dc
千代　就人 <chiyo@MacBookPro.lan>
2026-04-16 00:09:39 +0900

    4/15

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_09_141400) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_15_142936) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -59,9 +59,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_09_141400) do
     t.string "location"
     t.string "text"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "Profile"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -70,6 +72,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_09_141400) do
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
     t.datetime "updated_at", null: false
+    t.string "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
